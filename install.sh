5ca56dfddf97e614e024dccb661b30c7001062f5
Leonardo28l13 <lipao6912@gmail.com>
2026-02-15 16:26:25 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,53 +1,82 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# Define cores
+VERDE='\033[0;32m'
+NC='\033[0m'
+
+# Interrompe se houver erro
+set -e
+
+echo -e "${VERDE}Iniciando instalação...${NC}"
 
 cd /home
 
-git clone https://github.com/Leonardo28l13/Reverseproxy/tree/main
+# CORREÇÃO 1: Link do git correto (sem /tree/main)
+rm -rf /home/Reverseproxy # Limpa se já existir para evitar erro
+git clone https://github.com/Leonardo28113/Reverseproxy.git
 
-WINGSDIR="/srv/wings" && \
-mkdir $WINGSDIR && \
-cd $WINGSDIR && \
-LOCATION=$(curl -s https://api.github.com/repos/pterodactyl/wings/releases/latest \
-| grep "tag_name" \
-| awk '{print "https://github.com/pterodactyl/wings/archive/" substr($2, 2, length($2)-3) ".zip"}') \
+# Cria diretório do Wings
+WINGSDIR="/srv/wings"
+mkdir -p $WINGSDIR
+cd $WINGSDIR
 
-cd /home/Reverseproxy
+# CORREÇÃO 2: Baixa o código fonte do Wings corretamente
+echo -e "${VERDE}Baixando código fonte do Wings...${NC}"
+curl -L -o wings.zip $(curl -s https://api.github.com/repos/pterodactyl/wings/releases/latest | grep zipball_url | cut -d '"' -f 4)
+unzip -o wings.zip
+mv pterodactyl-wings-*/* .
+rm -rf pterodactyl-wings-* wings.zip
 
-cp -r router /srv/wings/wings-*
+# CORREÇÃO 3: Caminhos absolutos para evitar erros de cópia
+echo -e "${VERDE}Aplicando modificações...${NC}"
+cp -r /home/Reverseproxy/router/* $WINGSDIR/router/
+cp -f /home/Reverseproxy/router_server_proxy.go $WINGSDIR/router/
 
-cd /home/Reverseproxy
+# Instalação do GO
+echo -e "${VERDE}Instalando Go...${NC}"
+cd /home
+mkdir -p temp
+cd temp
 
-cp -r router.go /srv/wings/wings-*/router
-
-mkdir /home/temp
-
-cd /home/temp
-
-wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz && \
-rm -rf /usr/local/go && \
-tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz && \
+wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-cd /srv/wings/wings-*
+# Compilação
+echo -e "${VERDE}Compilando Wings (isso pode demorar)...${NC}"
+cd $WINGSDIR
 
-systemctl stop wings && \
-go get github.com/go-acme/lego/v4 && \
-go mod tidy && \
-go build -o /usr/local/bin/wings && \
-chmod +x /usr/local/bin/wings && \
+systemctl stop wings || true # O "|| true" impede erro se o wings não estiver rodando
+
+# Instala dependências e compila
+go get github.com/go-acme/lego/v4
+go mod tidy
+go build -o /usr/local/bin/wings
+chmod +x /usr/local/bin/wings
+
+# Reinicia o serviço
 systemctl start wings
 
-cd /home/Reverseproxy
+# Instalação do Blueprint
+# CORREÇÃO 4: Corrigido erro de digitação (pterodactyl)
+echo -e "${VERDE}Instalando Blueprint...${NC}"
+if [ -d "/var/www/pterodactyl" ]; then
+    cp -f /home/Reverseproxy/reverseproxy.blueprint /var/www/pterodactyl/
+    cd /var/www/pterodactyl
+    
+    # Verifica se o comando blueprint existe antes de rodar
+    if command -v blueprint &> /dev/null; then
+        blueprint -i reverseproxy
+    else
+        echo "Comando 'blueprint' não encontrado. Pulei esta etapa."
+    fi
+else
+    echo "Diretório /var/www/pterodactyl não encontrado."
+fi
 
-cp -r router_server_proxy.go /srv/wings/wings-*/router
-
-cd /home/Reverseproxy
-
-cp -r reverseproxy.blueprint /var/www/pterodatyl
-
-blueprint -i reverseproxy.blueprint
-
+# Limpeza
 rm -rf /home/Reverseproxy 
 rm -rf /home/temp
 
-echo -e "\033[32mInstalado com sucesso\033[0m"
+echo -e "${VERDE}Instalado com sucesso${NC}"
